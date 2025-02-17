e97b74543b6c789d6b623636da62b2edf9e804b0
Horatiu Cirstea <Horatiu.Cirstea@loria.fr>
2025-02-17 17:25:03 +0100

    remove warnings

--- src/main/java/org/lbee/network/ServerThread.java
+++ src/main/java/org/lbee/network/ServerThread.java
@@ -23,43 +23,40 @@ public class ServerThread extends Thread {
     @Override
     public void run() {
         try {
-            InputStream input = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-            OutputStream output = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(output, true);
-
-            String text;
-
-            do {
-                text = reader.readLine();
-
-                // Message send
-                if (text.startsWith("s:")) {
-                    // Get message data
-                    String messageData = text.substring(2, text.length());
-                    // Create message object from data
-                    Message message = new Message(messageData.split(";"));
-                    // Put message on queue
-                    networkMock.put(message);
-                    writer.println("ack");
-                    System.out.println("   Message sent: "+message);
-                }
-                // Message collect (receive)
-                else if (text.startsWith("r:")) {
-                    String dest = text.substring(2, text.length());
-                    // Search message
-                    Message message = networkMock.take(dest);
-                    // Send message to client that request it
-                    if (message != null)
-                        writer.println(message.toString());
-                    else
-                        writer.println("null");
-                }
-
-            } while (!text.equals("bye"));
-            // System.out.println("A client quit.");
-            socket.close();
+            try (socket) {
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+                String text;
+                do {
+                    text = reader.readLine();
+                    
+                    // Message send
+                    if (text.startsWith("s:")) {
+                        // Get message data
+                        String messageData = text.substring(2, text.length());
+                        // Create message object from data
+                        Message message = new Message(messageData.split(";"));
+                        // Put message on queue
+                        networkMock.put(message);
+                        writer.println("ack");
+                        System.out.println("   Message sent: "+message);
+                    }
+                    // Message collect (receive)
+                    else if (text.startsWith("r:")) {
+                        String dest = text.substring(2, text.length());
+                        // Search message
+                        Message message = networkMock.take(dest);
+                        // Send message to client that request it
+                        if (message != null)
+                            writer.println(message.toString());
+                        else
+                            writer.println("null");
+                    }
+                } while (!text.equals("bye"));
+                // System.out.println("A client quit.");
+            }
         } catch (IOException ex) {
             System.out.println("Server exception: " + ex.getMessage());
         }
